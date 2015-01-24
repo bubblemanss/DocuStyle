66262bcecb01354104f9816ba4585a5c234a98da
Maidi Lin <mswiz952@gmail.com>
2015-01-24 17:17:33 -0500

    nothing new

---
+++ main.js
@@ -0,0 +1,12 @@
+module.exports = function (iframe, PDFDocument, blobStream){
+    var doc = new PDFDocument();
+    var stream = doc.pipe(blobStream());
+
+    doc.text('You');
+
+    doc.end();
+    stream.on('finish', function(){
+        url = stream.toBlobURL('application/pdf');
+        iframe.src = url;
+    });
+}
